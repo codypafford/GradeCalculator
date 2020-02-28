44a0406deb16fe15617fbcb5bb0a2ae7fd4a6555
Cody Pafford <37154942+codypafford@users.noreply.github.com>
2020-02-27 19:39:26 -0500

    Add files via upload

--- GradeCalc/script.js
+++ GradeCalc/script.js
@@ -1,109 +1,33 @@
+function convert(number) {
+    if (!Number.isNaN(number)){
+        return number;
+    }
+    else{
+        return 0;
+    }
+}
+
 /**
  * @return {boolean}
  */
 function PointGrader() {
-    var grade1 = parseInt(document.getElementById("grade1_p").value);
-    var grade2 = parseInt(document.getElementById("grade2_p").value);
-    var grade3 = parseInt(document.getElementById("grade3_p").value);
-    var grade4 = parseInt(document.getElementById("grade4_p").value);
-    var grade5 = parseInt(document.getElementById("grade5_p").value);
-    var grade6 = parseInt(document.getElementById("grade6_p").value);
-    var grade7 = parseInt(document.getElementById("grade7_p").value);
-    var grade8 = parseInt(document.getElementById("grade8_p").value);
-    var grade9 = parseInt(document.getElementById("grade9_p").value);
-    var grade10 = parseInt(document.getElementById("grade10_p").value);
+    var total_points = 0;
+    var points_received = 0;
 
-    var weight1 = parseInt(document.getElementById("weight1_p").value);
-    var weight2 = parseInt(document.getElementById("weight2_p").value);
-    var weight3 = parseInt(document.getElementById("weight3_p").value);
-    var weight4 = parseInt(document.getElementById("weight4_p").value);
-    var weight5 = parseInt(document.getElementById("weight5_p").value);
-    var weight6 = parseInt(document.getElementById("weight6_p").value);
-    var weight7 = parseInt(document.getElementById("weight7_p").value);
-    var weight8 = parseInt(document.getElementById("weight8_p").value);
-    var weight9 = parseInt(document.getElementById("weight9_p").value);
-    var weight10 = parseInt(document.getElementById("weight10_p").value);
 
-    var final_grade = 0;
-    var counter = 0;
-    var grade = 0;
-
-    if(Number.isNaN(grade1)){
-        alert("Enter a number");
-        return false;
-    }else{
-        final_grade = (grade1 / weight1);
-        grade = grade + final_grade;
-        counter = counter + 1;
+    var i;
+    for (i = 1; i <= 10; i++) {
+        total_points = total_points + convert(parseInt(document.getElementById("weight" + i + "_p").value));
     }
 
-    // var x=document.getElementById('point_table');
-    // // deep clone the targeted row
-    // var new_row = x.rows[1].cloneNode(true);
-    // // get the total number of rows
-    // var len = x.rows.length;
-    //
-    // var inp1 = new_row.cells[1].getElementsByTagName('input')[0].value;
-    // var inp2 = new_row.cells[2].getElementsByTagName('input')[0].value;
-    //
-    //
-    // new_row.cells[0].innerHTML = '11';
+    var w;
+    for (w = 1; w <= 10; w++) {
+        points_received = points_received + convert(parseInt(document.getElementById("grade" + w + "_p").value));
+    }
 
 
-  //  alert(inp2);
-  //  x.append(new_row);
+    var final_grade = points_received/total_points;
 
-    if(!Number.isNaN(grade2) && !Number.isNaN(weight2)){
-        final_grade = (grade2 / weight2);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade3) && !Number.isNaN(weight3)){
-        final_grade = (grade3 / weight3);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade4) && !Number.isNaN(weight4)){
-        final_grade = (grade4 / weight4);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade5) && !Number.isNaN(weight5)){
-        final_grade = (grade5 / weight5);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade6) && !Number.isNaN(weight6)){
-        final_grade = (grade6 / weight6);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade7) && !Number.isNaN(weight7)){
-        final_grade = (grade7 / weight7);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade8) && !Number.isNaN(weight8)){
-        final_grade = (grade8 / weight8);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade9) && !Number.isNaN(weight9)){
-        final_grade = (grade9 / weight9);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    if(!Number.isNaN(grade10) && !Number.isNaN(weight10)){
-        final_grade = (grade10 / weight10);
-        grade = grade + final_grade;
-        counter = counter + 1;    }
-
-    final_grade = grade / counter;
-    final_grade = final_grade * 100;
-
-
-
-   // alert(final_grade);
     document.getElementById("your_grade").innerHTML = '<div align="center"><span> YOUR FINAL GRADE: ' +  final_grade + '%</span></div>';
 
 
@@ -124,98 +48,23 @@ function GetGrade(){
 }
 
 function myFunction(){
-    // var isPointChecked = document.getElementById('by_points').checked;
-    // if(isPointChecked){
-    //     PointGrader();
-    //     return false;
-    // }
-    var grade1 = parseInt(document.getElementById("grade1").value);
-    var grade2 = parseInt(document.getElementById("grade2").value);
-    var grade3 = parseInt(document.getElementById("grade3").value);
-    var grade4 = parseInt(document.getElementById("grade4").value);
-    var grade5 = parseInt(document.getElementById("grade5").value);
-    var grade6 = parseInt(document.getElementById("grade6").value);
-    var grade7 = parseInt(document.getElementById("grade7").value);
-    var grade8 = parseInt(document.getElementById("grade8").value);
-    var grade9 = parseInt(document.getElementById("grade9").value);
-    var grade10 = parseInt(document.getElementById("grade10").value);
 
-    var weight1 = parseInt(document.getElementById("weight1").value);
-    var weight2 = parseInt(document.getElementById("weight2").value);
-    var weight3 = parseInt(document.getElementById("weight3").value);
-    var weight4 = parseInt(document.getElementById("weight4").value);
-    var weight5 = parseInt(document.getElementById("weight5").value);
-    var weight6 = parseInt(document.getElementById("weight6").value);
-    var weight7 = parseInt(document.getElementById("weight7").value);
-    var weight8 = parseInt(document.getElementById("weight8").value);
-    var weight9 = parseInt(document.getElementById("weight9").value);
-    var weight10 = parseInt(document.getElementById("weight10").value);
 
     var final_grade;
     var final_weight_sums = 0;
 
 
-    if(Number.isNaN(grade1)){
-        alert("Enter a number");
-        return false;
-    }else{
-        final_grade = (grade1 * weight1);
-        final_weight_sums = final_weight_sums+ weight1;
+    var i;
+    for (i = 1; i <= 10; i++) {
+        var weight = convert(parseInt(document.getElementById("weight" + i).value));
+        final_grade = finall_grade + convert(parseInt(document.getElementById("grade" + i).value)) * weight;
+        final_weight_sums = final_weight_sums + weight;
     }
 
-    //use for loop to cycle through an array of pairs for this:
-
-    if(!Number.isNaN(grade2) && !Number.isNaN(weight2)){
-        final_grade = final_grade + (grade2 * weight2);
-        final_weight_sums = final_weight_sums + weight2;
-    }
-
-    if(!Number.isNaN(grade3) && !Number.isNaN(weight3)){
-        final_grade = final_grade + (grade3 * weight3);
-        final_weight_sums = final_weight_sums + weight3;
-    }
-
-    if(!Number.isNaN(grade4) && !Number.isNaN(weight4)){
-        final_grade = final_grade + (grade4 * weight4);
-        final_weight_sums = final_weight_sums + weight4;
-    }
-
-    if(!Number.isNaN(grade5) && !Number.isNaN(weight5)){
-        final_grade = final_grade + (grade5 * weight5);
-        final_weight_sums = final_weight_sums + weight5;
-    }
-
-    if(!Number.isNaN(grade6) && !Number.isNaN(weight6)){
-        final_grade = final_grade + (grade6 * weight6);
-        final_weight_sums = final_weight_sums + weight6;
-    }
-
-    if(!Number.isNaN(grade7) && !Number.isNaN(weight7)){
-        final_grade = final_grade + (grade7 * weight7);
-        final_weight_sums = final_weight_sums + weight7;
-    }
-
-    if(!Number.isNaN(grade8) && !Number.isNaN(weight8)){
-        final_grade = final_grade + (grade8 * weight8);
-        final_weight_sums = final_weight_sums + weight8;
-    }
-
-    if(!Number.isNaN(grade9) && !Number.isNaN(weight9)){
-        final_grade = final_grade + (grade9 * weight9);
-        final_weight_sums = final_weight_sums + weight5;
-    }
-
-    if(!Number.isNaN(grade10) && !Number.isNaN(weight10)){
-        final_grade = final_grade + (grade10 * weight10);
-        final_weight_sums = final_weight_sums + weight10;
-    }
 
     final_grade = final_grade/final_weight_sums;
 
 
-
-
-  //  alert(final_grade);
 
     document.getElementById("your_grade").innerHTML = '<div align="center"><span> YOUR FINAL GRADE: ' +  final_grade + '%</span></div>';
 
@@ -237,5 +86,4 @@ function SetTypeOfGrader(){
     }
 
 
-  //  alert(rate_value);
 }
